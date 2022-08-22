50623afd018c0534f359044bc9c3fbe2b6b4c8aa
Milo Somera <milo.somera@gmail.com>
2022-08-22 14:48:30 +0800

    add: set and remove storage functions; change: global states to get storage items

--- src/MyProvider.jsx
+++ src/MyProvider.jsx
@@ -8,18 +8,43 @@ const MyProvider = (props) => {
   const [user, setUser] = useState(
     localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
   );
-  const [recipeId, setRecipeId] = useState("");
-  const [recipeIngHref, setRecipeIngHref] = useState("");
+  const [searchKeyword, setSearchKeyword] = useState(
+    localStorage.getItem("searchKeyword")
+      ? JSON.parse(localStorage.getItem("searchKeyword"))
+      : ""
+  );
+  const [recipeId, setRecipeId] = useState(
+    localStorage.getItem("recipeId")
+      ? JSON.parse(localStorage.getItem("recipeId"))
+      : ""
+  );
+  const [recipeIngHref, setRecipeIngHref] = useState(
+    localStorage.getItem("recipeIngHref")
+      ? JSON.parse(localStorage.getItem("recipeIngHref"))
+      : ""
+  );
+
+  const setStorageItems = (items) => {
+    items.forEach((item) => localStorage.setItem(item[0], item[1]));
+  };
+
+  const removeStorageItems = (items) => {
+    items.forEach((item) => localStorage.removeItem(item));
+  };
 
   const state = {
     isLoggedIn,
     user,
+    searchKeyword,
     recipeId,
     recipeIngHref,
     setIsLoggedIn,
     setUser,
+    setSearchKeyword,
     setRecipeId,
     setRecipeIngHref,
+    setStorageItems,
+    removeStorageItems,
   };
   return (
     <MyContext.Provider value={state}>{props.children}</MyContext.Provider>
