47dec03e785470f0baa9c77240c1f8d5b27a530b
Milo Somera <milo.somera@gmail.com>
2022-08-23 18:01:34 +0800

    change: images size from 300px to 600px; remove top padding on carousel

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -8,7 +8,7 @@ import shrimp from "../images/shrimp.jpg";
 
 const CarouselComponent = () => {
   return (
-    <Carousel className="carousel-component mx-auto mt-5 pt-5" fade>
+    <Carousel className="carousel-component mx-auto" fade>
       <Carousel.Item interval={3000}>
         <img
           className="d-block w-100 rounded-2"
@@ -16,7 +16,7 @@ const CarouselComponent = () => {
           alt="Roast sirloin of beef"
         />
         <Carousel.Caption>
-          <div>Roast sirloin of beef</div>
+          <h2>Roast sirloin of beef</h2>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={3000}>
@@ -26,7 +26,7 @@ const CarouselComponent = () => {
           alt="Catalan chicken"
         />
         <Carousel.Caption>
-          <div>Catalan chicken</div>
+          <h2>Catalan chicken</h2>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={3000}>
@@ -36,13 +36,13 @@ const CarouselComponent = () => {
           alt="Fish with peas and lettuce"
         />
         <Carousel.Caption>
-          <div>Fish with peas and lettuce</div>
+          <h2>Fish with peas and lettuce</h2>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={3000}>
         <img className="d-block w-100 rounded-2" src={pork} alt="Pork chops" />
         <Carousel.Caption>
-          <div>Pork chops</div>
+          <h2>Pork chops</h2>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={3000}>
@@ -52,7 +52,7 @@ const CarouselComponent = () => {
           alt="Piri piri shrimp"
         />
         <Carousel.Caption>
-          <div>Piri piri shrimp</div>
+          <h2>Piri piri shrimp</h2>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
